41b9869c434b9073119f2aee60a61f1277cfd623
robertojff <robertojff3333@gmail.com>
2023-12-18 18:00:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//Realizar um tarefa , não devolve nada
+function dizerNome(){
+    console.log('Roberto');
+}
+dizerNome();
+// Faz um cálculo ou operação e retorna algo 
+function MultiplicarPorDois(valor){
+    return valor* 2;
+}
+
+// console.log(MultiplicarPorDois(5));
+
+let resultado = MultiplicarPorDois(5);
+
+console.log(resultado);
